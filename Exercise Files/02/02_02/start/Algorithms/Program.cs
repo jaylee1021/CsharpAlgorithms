539ac95c0a8826db279e821d87dc4a22903fc200
Jay Lee <randomdori@gmail.com>
2024-03-22 13:21:24 -0700

    binary search 2-02

--- Exercise Files/02/02_02/start/Algorithms/Program.cs
+++ Exercise Files/02/02_02/start/Algorithms/Program.cs
@@ -1,14 +1,40 @@
 ﻿using System;
 
-namespace Algorithms {
-    class Program {
+namespace Algorithms
+{
+    class Program
+    {
 
-        static Boolean BinarySearch(int[] inputArray, int item) {
+        static Boolean BinarySearch(int[] inputArray, int item)
+        {
+            int min = 0;
+            int max = inputArray.Length - 1;
+
+            while (min <= max)
+            {
+                int mid = (min + max) / 2;
+                if (item == inputArray[mid])
+                {
+                    return true;
+                }
+                else if (item < inputArray[mid])
+                {
+                    max = mid - 1;
+                }
+                else
+                {
+                    min = mid + 1;
+                }
+            }
+
             return false;
         }
 
-        static void Main(string[] args) {
-            int[] arr = {1, 2, 3, 4, 5, 6 };
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6 };
+            Console.WriteLine(BinarySearch(arr, 5));
+            Console.WriteLine(Array.BinarySearch(arr, 3));
         }
     }
 }
