c2573993d17888bb3e2ccb21aa04bd0f37f5f33b
Jay Lee <randomdori@gmail.com>
2024-03-22 10:43:58 -0700

    validate string

--- Exercise Files/00/00_04/start/Algorithms/obj/Debug/netcoreapp2.0/Algorithms.AssemblyInfo.cs
+++ Exercise Files/00/00_04/start/Algorithms/obj/Debug/netcoreapp2.0/Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3488bdc70bab8ea568b30240bc67e2de57806e2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d280282a0b7cb3d865aa5dcda8880892e0a874")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Exercise Files/01/01_01/start/Algorithms/Program.cs
+++ Exercise Files/01/01_01/start/Algorithms/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
-
+using System.Linq;
 namespace Algorithms
 {
     class Program
     {
+
+        static Boolean IsUppercase(string s)
+        {
+            return s.All(char.IsUpper);
+        }
+
+        static Boolean IsLowercase(string s)
+        {
+            return s.All(char.IsLower);
+        }
+
+        static Boolean IsPasswordComplex(string s)
+        {
+            return s.Any(char.IsUpper) && s.Any(char.IsLower) && s.Any(char.IsDigit);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // Console.WriteLine(IsUppercase("hello"));
+            // Console.WriteLine(IsUppercase("HELLO"));
+
+            // Console.WriteLine(IsLowercase("hello"));
+            // Console.WriteLine(IsLowercase("HELLO"));
+
+            Console.WriteLine(IsPasswordComplex("Hell0"));
+            Console.WriteLine(IsPasswordComplex("Hello"));
+            Console.WriteLine(IsPasswordComplex("HellO"));
+            Console.WriteLine(IsPasswordComplex("HeLlo"));
+            Console.WriteLine(IsPasswordComplex("hello"));
+            Console.WriteLine(IsPasswordComplex(" "));
         }
 
     }
