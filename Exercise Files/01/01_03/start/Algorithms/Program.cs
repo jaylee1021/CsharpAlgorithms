466f8e92abe83d76e1514ecb1e9b6a509e0ad5ea
Jay Lee <randomdori@gmail.com>
2024-03-22 11:11:03 -0700

    parse string

--- Exercise Files/01/01_03/start/Algorithms/Program.cs
+++ Exercise Files/01/01_03/start/Algorithms/Program.cs
@@ -5,9 +5,49 @@ namespace Algorithms
     class Program
     {
 
+        static void ParseContents(string s)
+        {
+            Console.WriteLine("Option 1");
+            foreach (char c in s)
+            {
+                Console.WriteLine(c);
+            }
+
+            Console.WriteLine("Option 2");
+            for (int i = 0; i < s.Length; i++)
+            {
+                char c = s[i];
+                Console.WriteLine(c);
+            }
+        }
+
+        static Boolean IsAtEvenIndex(String s, char item)
+        {
+            if (String.IsNullOrEmpty(s))
+            {
+                return false;
+            }
+
+            for (int i = 0; i < s.Length; i += 2)
+            {
+                if (s[i] == item)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            // Console.WriteLine("Hello World".ToLower().Contains("ll"));
+            // ParseContents("Hello World");
+            String input = "HellL0";
+            Console.WriteLine(IsAtEvenIndex(input, 'L'));
+            Console.WriteLine(IsAtEvenIndex(input, 'T'));
+            Console.WriteLine(IsAtEvenIndex(input, 'H'));
+            Console.WriteLine(IsAtEvenIndex("", 'H'));
+            Console.WriteLine(IsAtEvenIndex(null, 'H'));
         }
     }
 }
--- Exercise Files/05/05_05_Challenge/start/Algorithms/obj/Debug/netcoreapp2.0/Algorithms.AssemblyInfo.cs
+++ Exercise Files/05/05_05_Challenge/start/Algorithms/obj/Debug/netcoreapp2.0/Algorithms.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82d280282a0b7cb3d865aa5dcda8880892e0a874")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e46da2f225c7ff4f7cabe7a0f9f2efdc0088990f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Algorithms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
