e3488bdc70bab8ea568b30240bc67e2de57806e2
Jay Lee <randomdori@gmail.com>
2024-03-22 08:52:01 -0700

    update

--- Exercise Files/00/00_04/start/Algorithms/Program.cs
+++ Exercise Files/00/00_04/start/Algorithms/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 
-namespace Algorithms {
+namespace Algorithms
+{
 
-    class Program {
-        static int findMaximum(int a, int b, int c) {
-            if (a > b) {
-                if (a > c || a == c) {
+    class Program
+    {
+        static int findMaximum(int a, int b, int c)
+        {
+            if (a > b)
+            {
+                if (a > c || a == c)
+                {
                     return a;
                 }
             }
 
-            if (b > c) {
-                if (b > a || a == b) {
+            if (b > c)
+            {
+                if (b > a || a == b)
+                {
                     return b;
                 }
             }
@@ -19,12 +26,30 @@ namespace Algorithms {
             return c;
         }
 
-        static void Main(string[] args) {
-            Console.WriteLine(findMaximum(1, 2, 3));
-            Console.WriteLine(findMaximum(8, 8, 1));
-            Console.WriteLine(findMaximum(3, 2, 3));
-            Console.WriteLine(findMaximum(1, 1, 9));
-            Console.WriteLine(findMaximum(1, 9, 9));
+        static int findMaximum2(int a, int b, int c)
+        {
+            int max = a;
+
+            if (b > max)
+            {
+                max = b;
+            }
+
+            if (c > max)
+            {
+                max = c;
+            }
+
+            return max;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(findMaximum2(1, 2, 3));
+            Console.WriteLine(findMaximum2(8, 8, 1));
+            Console.WriteLine(findMaximum2(3, 2, 3));
+            Console.WriteLine(findMaximum2(1, 1, 9));
+            Console.WriteLine(findMaximum2(1, 9, 9));
         }
     }
 }
